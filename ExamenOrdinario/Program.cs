4b080ea38548a35f0a8a9d97b6fa7b7b0417e107
Rbn <rubenavalosmtz@gmail.com>
2023-12-18 19:28:50 -0600

    Agregar archivos de proyecto.

---
+++ ExamenOrdinario/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenOrdinario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
