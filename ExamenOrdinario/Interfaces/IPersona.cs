59019f3decf0b0b3e08908901d12720ed0672bb8
SalazarDaniel <danielsalazarberzunza@hotmail.com>
2023-12-19 08:34:41 -0600

    Añadí la propiedad ID

---
+++ ExamenOrdinario/Interfaces/IPersona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenOrdinario.Interfaces
+{
+    public interface IPersona
+    {
+        public string ID
+        {
+            get; 
+        }
+
+    }
+}
