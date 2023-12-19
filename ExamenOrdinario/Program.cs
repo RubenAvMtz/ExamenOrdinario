7d0b762c95e61b6cddc4df39cc48f5289d904f7b
SalazarDaniel <danielsalazarberzunza@hotmail.com>
2023-12-19 09:44:49 -0600

    Añadí la función del Menu Administración Centro
    Se implementaron también los menús rama del Menu Adminsitración Centro

--- ExamenOrdinario/Program.cs
+++ ExamenOrdinario/Program.cs
@@ -17,6 +17,7 @@
                 switch(opcionSeleccionadaMP)
                 {
                     case 1:
+                        MenuAdministacionCentro();
                         break;
                     case 2:
                         break;
@@ -26,6 +27,93 @@
                         break;
                     case 5:
                         continuarPrograma = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        break;
+                }
+            }
+        }
+
+        static void MenuAdministacionCentro()
+        {
+            bool continuarMAC = true; // MAC =  Menu Administracion Centro
+            while (continuarMAC)
+            {
+                Console.WriteLine("1 - Administración de personas");
+                Console.WriteLine("2 - Administración de mascotas");
+                Console.WriteLine("3 - Regresar al menú anterior");
+                int opcionSeleccionadaMAC = int.Parse(Console.ReadLine());
+                switch(opcionSeleccionadaMAC)
+                {
+                    case 1:
+                        MenuCentroAdministracionPersonas();
+                        break;
+                    case 2:
+                        MenuCentroAdministracionMascotas();
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        break;
+                }
+            }
+        }
+
+        static void MenuCentroAdministracionPersonas()
+        {
+            bool continuarMCAP = true; // MCAP = Menu Centro Administracion Personas
+            while (continuarMCAP)
+            {
+                Console.WriteLine("1 - Mostrar las personas registradas");
+                Console.WriteLine("2 - Registrar persona nueva");
+                Console.WriteLine("3 - Buscar personas por nombre");
+                Console.WriteLine("4 - Examinar Persona");
+                Console.WriteLine("5 - Regresar al menu anterior");
+                int opcionSeleccionadaMCAP = int.Parse(Console.ReadLine());
+                switch (opcionSeleccionadaMCAP)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        break;
+                }
+            }
+        }
+
+        static void MenuCentroAdministracionMascotas()
+        {
+            bool continuarMCAM = true; // MCAM = Menu Centro Administracion Mascotas
+            while (continuarMCAM)
+            {
+                Console.WriteLine("1 - Mostrar todas las mascotas registradas");
+                Console.WriteLine("2 - Registrar mascota nueva");
+                Console.WriteLine("3 - Buscar mascotas por especie");
+                Console.WriteLine("4 - Buscar mascotas por nombre");
+                Console.WriteLine("5 - Examinar mascota");
+                Console.WriteLine("6 - Regresar al menu anterior");
+                int opcionSeleccionadaMCAM = int.Parse(Console.ReadLine());
+                switch (opcionSeleccionadaMCAM)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
                         break;
                     default:
                         Console.WriteLine("Opcion invalida");
