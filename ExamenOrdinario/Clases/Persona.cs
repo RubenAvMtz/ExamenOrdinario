48d658d105eca9fddfb8a0fd7b28df4b63f33200
SalazarDaniel <danielsalazarberzunza@hotmail.com>
2023-12-19 09:06:18 -0600

    Añadí la clase Persona

---
+++ ExamenOrdinario/Clases/Persona.cs
@@ -0,0 +1,16 @@
+﻿using ExamenOrdinario.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenOrdinario.Clases
+{
+    public class Persona : IPersona
+    {
+        public int ID => throw new NotImplementedException();
+
+        public string Nombre { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
